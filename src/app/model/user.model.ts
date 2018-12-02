cebbd5a2b407fb3588c833c7bcaf66925dff87d8
unknown <andrescassanaz@gmail.com>
2018-12-02 16:22:53 -0300

    Se corrige entidad user

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,9 +1,7 @@
-import {UserRoleModel} from "./userRole.model";
+
 
 export class UserModel {
     public username: string;
     public password: string;
-    public type: number;
     public token: string;
-    public userRole:UserRoleModel[];
 }
